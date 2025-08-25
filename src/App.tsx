ef5dc5ac5a01b8519fea85e237a49f02fa1f6720
mookp12 <kraiviwat.p@gmail.com>
2025-08-25 11:23:50 +0700

    moo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-
+import Greeting from "./Greeting";
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return <Greeting name="John" age={20} />;
 }
 
 export default App;
